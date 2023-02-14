ae41247069f2ce64ffcb24b79c545693c7ad04cf
Pree-byte <65903367+Pree-byte@users.noreply.github.com>
2023-02-14 21:56:13 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,6 +1,3 @@
 echo "This is demo"
 
-docker login acrpreet.azurecr.io  --username $username --password $password
-
-az acr repository list -n $registryname
-
+docker login --tty acrpreet.azurecr.io  --username $username --password $password
