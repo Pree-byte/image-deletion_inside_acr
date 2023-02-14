1c4b249d485d9536b9efdc7c4f1902fcf5d6a8f1
Pree-byte <65903367+Pree-byte@users.noreply.github.com>
2023-02-14 23:30:00 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,35 @@
-echo "This is demo"
+#!/bin/bash
 
-docker login --tty acrpreet.azurecr.io  --username $username --password $password
+# WARNING! This script deletes data!
+# Run only if you do not have systems
+# that pull images via manifest digest.
+
+# Change to 'true' to enable image delete
+ENABLE_DELETE=true
+
+# Modify for your environment
+REGISTRY=$1
+TIMESTAMP=$2
+
+# Delete all images older than specified timestamp in all repositories.
+
+if [ "$ENABLE_DELETE" = true ]
+then
+    for REPOSITORY in $(az acr repository list --name $REGISTRY --query "[]" -o tsv)
+    do
+        az acr manifest list-metadata --name $REPOSITORY --registry $REGISTRY \
+        --orderby time_asc --query "[?lastUpdateTime < '$TIMESTAMP'].digest" -o tsv \
+        | xargs -I% az acr repository delete --name $REGISTRY --image $REPOSITORY@% --yes
+    done
+else
+    echo "No data deleted."
+    echo "Set ENABLE_DELETE=true to enable deletion of these images in all repositories:"
+    for REPOSITORY in $(az acr repository list --name $REGISTRY --query "[].name" -o tsv)
+    do
+        echo "Repository: $REPOSITORY"
+        az acr manifest list-metadata --name $REPOSITORY --registry $REGISTRY \
+       --orderby time_asc --query "[?lastUpdateTime < '$TIMESTAMP'].[digest, lastUpdateTime]" -o tsv
+    done
+fi
+
+
